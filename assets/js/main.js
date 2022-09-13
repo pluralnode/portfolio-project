3131c465a40741eb9c39a23420a49ce94deb268b
pluralnode <robowiregh@gmail.com>
2022-09-13 13:40:26 +0100

    modified html and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 const navBlock = document.querySelector('.nav__block');
 const navMenu = document.querySelector('.nav__menu');
-console.log(navBlock);
-navBlock.addEventListener('click', () => {
+
+const handleClick = (e) => {
     navBlock.classList.toggle('active');
     navMenu.classList.toggle('active__menu');
-});
+}
+
+
+navBlock.addEventListener('click', handleClick);
